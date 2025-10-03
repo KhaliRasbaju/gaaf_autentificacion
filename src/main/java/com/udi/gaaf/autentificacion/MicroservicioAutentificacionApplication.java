cc5039c9918890b88166ae42f58f810271e3b9df
KhaliRasbaju <ksalamanca1@udi.edu.co>
2025-10-03 15:28:18 -0500

    Microservicio de autentificación

---
+++ src/main/java/com/udi/gaaf/autentificacion/MicroservicioAutentificacionApplication.java
@@ -0,0 +1,15 @@
+package com.udi.gaaf.autentificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroservicioAutentificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioAutentificacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udi/gaaf/autentificacion/MicroservicioAutentificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udi.gaaf.autentificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAutentificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
