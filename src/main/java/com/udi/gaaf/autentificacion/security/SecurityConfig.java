396c97a4416e57e0fbd821a65d8aa77c14a449c1
KhaliRasbaju <ksalamanca1@udi.edu.co>
2025-10-05 12:04:38 -0500

    Cambios en el dockerfile

--- src/main/java/com/udi/gaaf/autentificacion/security/SecurityConfig.java
+++ src/main/java/com/udi/gaaf/autentificacion/security/SecurityConfig.java
@@ -1,82 +1,82 @@
-package com.udi.gaaf.autentificacion.security;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.udi.gaaf.autentificacion.errors.CustomAuthenticationEntryPoint;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
+	package com.udi.gaaf.autentificacion.security;
 	
-	@Autowired
-	private SecurityFilter securityFilter;
+	import java.util.HashMap;
+	import java.util.Map;
 	
-	@Autowired
-	private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.http.HttpStatus;
+	import org.springframework.security.authentication.AuthenticationManager;
+	import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+	import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+	import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+	import org.springframework.security.config.http.SessionCreationPolicy;
+	import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+	import org.springframework.security.crypto.password.PasswordEncoder;
+	import org.springframework.security.web.SecurityFilterChain;
+	import org.springframework.security.web.access.AccessDeniedHandler;
+	import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 	
+	import com.fasterxml.jackson.databind.ObjectMapper;
+	import com.udi.gaaf.autentificacion.errors.CustomAuthenticationEntryPoint;
 	
-	@Bean
-	public SecurityFilterChain secureFilterChain(HttpSecurity http) throws Exception{
-		return http.csrf(crsf -> crsf.disable())
-				.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(req -> {
-					req.requestMatchers("/auth/*").permitAll();
-					req.anyRequest().authenticated();
-				})
-				.exceptionHandling(exceptions -> {
-					exceptions.authenticationEntryPoint(customAuthenticationEntryPoint);
-					exceptions.accessDeniedHandler(accessDeniedHandler());
-				})
-				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-				.build();
+	import jakarta.servlet.http.HttpServletResponse;
+	
+	@Configuration
+	@EnableWebSecurity
+	public class SecurityConfig {
+	
+		
+		@Autowired
+		private SecurityFilter securityFilter;
+		
+		@Autowired
+		private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+		
+		
+		@Bean
+		public SecurityFilterChain secureFilterChain(HttpSecurity http) throws Exception{
+			return http.csrf(crsf -> crsf.disable())
+					.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.authorizeHttpRequests(req -> {
+						req.requestMatchers("/auth/*").permitAll();
+						req.anyRequest().authenticated();
+					})
+					.exceptionHandling(exceptions -> {
+						exceptions.authenticationEntryPoint(customAuthenticationEntryPoint);
+						exceptions.accessDeniedHandler(accessDeniedHandler());
+					})
+					.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+					.build();
+		}
+		
+		
+		
+		@Bean 
+		public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+			return authenticationConfiguration.getAuthenticationManager();
+		}
+		
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+		
+		private AccessDeniedHandler accessDeniedHandler() {
+			return (request, response, accessDeniedException) -> {
+				 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		            response.setContentType("application/json");
+		            response.setCharacterEncoding("UTF-8");
+		            Map<String, Object> json = new HashMap<>();
+		            json.put("status", HttpStatus.UNAUTHORIZED.value());
+		            json.put("message", "No tienes permisos suficientes");
+		            ObjectMapper objectMapper = new ObjectMapper();
+		            response.getWriter().write(
+		                    objectMapper.writeValueAsString(json));
+		            response.getWriter().flush();
+			};
+		}
+		
+		
 	}
-	
-	
-	
-	@Bean 
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-		return authenticationConfiguration.getAuthenticationManager();
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	private AccessDeniedHandler accessDeniedHandler() {
-		return (request, response, accessDeniedException) -> {
-			 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-	            response.setContentType("application/json");
-	            response.setCharacterEncoding("UTF-8");
-	            Map<String, Object> json = new HashMap<>();
-	            json.put("status", HttpStatus.UNAUTHORIZED.value());
-	            json.put("message", "No tienes permisos suficientes");
-	            ObjectMapper objectMapper = new ObjectMapper();
-	            response.getWriter().write(
-	                    objectMapper.writeValueAsString(json));
-	            response.getWriter().flush();
-		};
-	}
-	
-	
-}
